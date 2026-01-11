d0d9756065e8104ce3bb8ff7a23e7a5c5b78414f
ThatFinn <finn@thatfinn.dev>
2026-01-11 20:42:56 +0100

    Menu Example
    Add Menu example
    Add automatic compilation

---
+++ Menu Example/ExpansionEntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Il2CppMonomiPark.SlimeRancher;
+using SR2E.Expansion;
+using SR2E.Saving;
+using SR2E.Storage;
+using SR2E.Utils;
+
+namespace ExampleExpansion;
+
+
+public class ExampleExpansionEntrypoint : SR2EExpansionV3
+{
+    public override void OnInitializeMelon()
+    {
+        AddLanguages(EmbeddedResourceEUtil.LoadString("translations.csv"));
+    }
+}
+
